67c68333e5c2776bc483ed3ba49b5f19187b60d9
sudipsingh04 <sudipsingh04@gmail.com>
2020-05-12 13:45:16 +0530

    Added relationships

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -2,9 +2,23 @@
 
 namespace App\Model;
 
+use App\User;
+use App\Model\Reply;
+use App\Model\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+    public function category()
+    {
+        $this->belongsTo(Category::class);
+    }
 }
