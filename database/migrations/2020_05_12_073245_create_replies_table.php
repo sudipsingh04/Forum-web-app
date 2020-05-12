dc639f44770851f35a20329c4b7b300771c1a988
sudipsingh04 <sudipsingh04@gmail.com>
2020-05-12 13:28:07 +0530

    Fixed issue in reply table

--- database/migrations/2020_05_12_073245_create_replies_table.php
+++ database/migrations/2020_05_12_073245_create_replies_table.php
@@ -16,7 +16,7 @@ class CreateRepliesTable extends Migration
         Schema::create('replies', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->text('body');
-            $table->integer('question_id')->unsigned();
+            $table->unsignedBigInteger('question_id');
             $table->integer('user_id');
             $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
             $table->timestamps();
