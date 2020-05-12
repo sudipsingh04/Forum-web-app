0cbeacba4e91a88c5026a568c4076c979bc8c8e2
sudipsingh04 <sudipsingh04@gmail.com>
2020-05-12 14:20:07 +0530

    Added factories

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -2,11 +2,14 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\User;
 use App\Model\Like;
 use Faker\Generator as Faker;
 
 $factory->define(Like::class, function (Faker $faker) {
     return [
-        //
+        'user_id'       => function(){
+            return User::all()->random();
+        }
     ];
 });
