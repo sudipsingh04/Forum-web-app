f5a68d3d2b3f8f6459d7c8f2f28c5322c6305a9d
sudipsingh04 <sudipsingh04@gmail.com>
2020-05-12 18:38:04 +0530

    Modified question resource controller model

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -9,9 +9,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    protected $guarded = [];
+
     public function user()
     {
-        $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);
     }
     public function replies()
     {
@@ -19,6 +26,10 @@ class Question extends Model
     }
     public function category()
     {
-        $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class);
+    }
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
