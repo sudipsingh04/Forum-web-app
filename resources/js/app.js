1bf2aa74eb1cf7e44cfb48da17bd7f013efdb57f
sudipsingh04 <sudipsingh04@gmail.com>
2020-05-17 13:24:23 +0530

    Vuetify, vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,8 +23,8 @@ Vue.use(Vuetify)
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
+Vue.component('AppHome', require('./components/AppHome.vue').default);
+import router from './Router/router.js'
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -34,4 +34,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
     vuetify: new Vuetify(),
+    router
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <title>Document</title>
+    <title>SPA Forum</title>
 </head>
 <body>
     <div id="app">
         <v-app>
-
+            <app-home/>
         </v-app>
     </div>
     <script src="{{asset('js/app.js')}}"></script>
