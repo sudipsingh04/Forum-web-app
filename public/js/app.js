7fa71dc36a5be3a62d707f398c71bfeb9709cdbd
sudipsingh04 <sudipsingh04@gmail.com>
2020-05-23 17:00:45 +0530

    Pass token

--- public/js/app.js
+++ public/js/app.js
@@ -114385,6 +114385,8 @@ try {
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+var JwtToken = "Bearer ".concat(localStorage.getItem('token'));
+window.axios.defaults.headers.common['Authorization'] = JwtToken;
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
