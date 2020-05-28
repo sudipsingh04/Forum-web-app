33c5bb7d8f561c79932e29d4fa18dc0598c4f8fc
sudipsingh04 <sudipsingh04@gmail.com>
2020-05-28 20:08:32 +0530

    Handle random token

--- resources/js/Helpers/Token.js
+++ resources/js/Helpers/Token.js
@@ -9,7 +9,19 @@ class Token{
 
     payload(token){
         const payload = token.split('.')[1]
-        return JSON.parse(atob(payload));
+
+        if(this.isBase64(payload)){
+            return JSON.parse(atob(payload));
+        }
+        return false
+    }
+
+    isBase64(str){
+        try {
+            return btoa(atob(str)).replace(/=/g,"") == str
+        } catch (error) {
+            return false
+        }
     }
 }
 
