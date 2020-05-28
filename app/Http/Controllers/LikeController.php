c2eebfdabeb58ba802b65919321d04995506bde7
sudipsingh04 <sudipsingh04@gmail.com>
2020-05-28 12:22:38 +0530

    Real time likes

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Model\Like;
 use App\Model\Reply;
+use App\Events\LikeEvent;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
@@ -18,10 +19,14 @@ class LikeController extends Controller
         $reply->like()->create([
             'user_id'   => auth()->id()
         ]);
+
+        broadcast(new LikeEvent($reply->id, 1))->toOthers();
     }
 
     public function unLikeIt(Reply $reply)
     {
         $reply->like()->where('user_id', auth()->id())->first()->delete();
+
+        broadcast(new LikeEvent($reply->id, 0))->toOthers();
     }
 }
