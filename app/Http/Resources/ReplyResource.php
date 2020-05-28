b7a0aa8e61c402def30da458b4012c61ed58c859
sudipsingh04 <sudipsingh04@gmail.com>
2020-05-28 19:43:54 +0530

    Fixed reply resource

--- app/Http/Resources/ReplyResource.php
+++ app/Http/Resources/ReplyResource.php
@@ -21,7 +21,7 @@ class ReplyResource extends JsonResource
             'user_id'       => $this->user_id,
             'question_slug' => $this->question->slug,
             'like_count'    => $this->like->count(),
-            'liked'         => !! $this->like->where('user_id', auth()->user()->id)->count(),
+            'liked'         => auth()->user() ? !! $this->like->where('user_id', auth()->user()->id)->count() : '',
             'created_at'    => $this->created_at->diffForHumans()
         ];
     }
